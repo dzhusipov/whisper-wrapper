7161ba6f687bf87ff6b33cea155c8a387cb49301
DAsm <zh.daur@gmail.com>
2023-10-30 16:12:35 +0600

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-use amiquip::{Connection, ConsumerMessage, ConsumerOptions, QueueDeclareOptions, Publish, ExchangeDeclareOptions, ExchangeType};
+use amiquip::{
+    Connection, ConsumerMessage, ConsumerOptions, ExchangeDeclareOptions, ExchangeType, Publish,
+    QueueDeclareOptions,
+};
 use log::{debug, error};
 use std::fs::File;
 use std::io::prelude::*;
 use std::process::Command;
 
-
-fn main()  {
+fn main() {
     log4rs::init_file("config/log4rs.yaml", Default::default()).unwrap();
 
     let dotenvy_res = dotenvy::dotenv();
@@ -20,19 +22,30 @@ fn main()  {
     let rabbit_host = dotenvy::var("RABBIT_HOST").unwrap();
     let rabbit_port = dotenvy::var("RABBIT_PORT").unwrap();
 
-    let rabbit_conn_url = format!("amqp://{rabbit_login}:{rabbit_password}@{rabbit_host}:{rabbit_port}");
+    let rabbit_conn_url =
+        format!("amqp://{rabbit_login}:{rabbit_password}@{rabbit_host}:{rabbit_port}");
     // Connect to RabbitMQ
     let mut connection = Connection::insecure_open(rabbit_conn_url.as_str()).unwrap();
     let channel = connection.open_channel(None).unwrap();
-    
+
     let sender_channel = connection.open_channel(None).unwrap();
 
-    let _sender_queue = sender_channel.queue_declare("transcription_results", QueueDeclareOptions::default()).unwrap();
+    let _sender_queue = sender_channel
+        .queue_declare("transcription_results", QueueDeclareOptions::default())
+        .unwrap();
 
     // Declare the transcription queue
-    let queue = sender_channel.queue_declare("files_to_transcribe", QueueDeclareOptions::default()).unwrap();
-    
-    let _results_exchange = channel.exchange_declare(ExchangeType::Direct, "fanout", ExchangeDeclareOptions::default()).unwrap();
+    let queue = sender_channel
+        .queue_declare("files_to_transcribe", QueueDeclareOptions::default())
+        .unwrap();
+
+    let _results_exchange = channel
+        .exchange_declare(
+            ExchangeType::Direct,
+            "fanout",
+            ExchangeDeclareOptions::default(),
+        )
+        .unwrap();
 
     // Start consuming messages from the queue
     let consumer = queue.consume(ConsumerOptions::default()).unwrap();
@@ -40,7 +53,8 @@ fn main()  {
         match message {
             ConsumerMessage::Delivery(delivery) => {
                 let headers = delivery.properties.headers().as_ref();
-                let filename_amqp: Option<&amiquip::AmqpValue> = headers.expect("empty_filename").get("filename");
+                let filename_amqp: Option<&amiquip::AmqpValue> =
+                    headers.expect("empty_filename").get("filename");
                 let filename: &amiquip::AmqpValue = filename_amqp.unwrap();
                 let filename_string = match filename {
                     amiquip::AmqpValue::LongString(s) => s.clone(),
@@ -72,14 +86,14 @@ fn main()  {
 
                 // ./main -otxt -m models/ggml-base.en.bin -f samples/jfk.wav
                 let whisper = Command::new("./main")
-                    .arg("-oj")         // json output
+                    .arg("-oj") // json output
                     .arg("-m")
                     .arg("models/ggml-base.bin")
                     .arg("-f")
                     .arg(filename_string.clone())
                     .output()
                     .unwrap();
-                
+
                 debug!("{}", String::from_utf8_lossy(&whisper.stdout));
                 // Publish a message to the results queue
                 let publish: Publish = Publish::new("Hello World".as_bytes(), "");
@@ -88,7 +102,7 @@ fn main()  {
                     Ok(_) => debug!("Message sent"),
                     Err(e) => error!("Error sending message: {}", e),
                 }
-                // Acknowledge the message                
+                // Acknowledge the message
                 consumer.ack(delivery).unwrap();
             }
             other => {
